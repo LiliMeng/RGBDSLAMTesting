6494d7476c1c4445f25cfbef5f71c902ffd28344
LiliMeng <lilimeng1103@gmail.com>
2015-12-23 16:44:28 -0800

    Create readandwriteRGBD.cpp

---
+++ readandwriteRGBD.cpp
@@ -0,0 +1,43 @@
+ //only part of read and write RGBD info.
+ 
+ cv::Mat readDepthImage(const char *fileName)
+{
+    cv::Mat depth = cv::imread(fileName, -1);
+    assert(depth.type() == 2);
+    return depth;
+}
+ 
+ 
+ 
+ {   // save frame
+            char fileName[1024] = {NULL};
+            sprintf(fileName, "frame_depth_%06d.png", frame_index);
+            cv::imwrite(fileName, depth);
+            frame_index ++;
+            // save frame
+            char fileName[1024] = {NULL};
+            sprintf(fileName, "frame_depth_%06d.png", frame_index);
+            cv::imwrite(fileName, depth);
+            frame_index ++;
+
+            // read from saved image
+            cv::Mat depth2 = readDepthImage(fileName);
+            assert(depth.type() == depth2.type());
+
+            for(int y = 0; y < depth.rows; y++)
+            {
+                for(int x = 0; x< depth.cols; x++)
+                {
+                    if(depth.at<unsigned short>(y, x) != depth2.at<unsigned short>(y, x))
+                    {
+                        printf("depth depth2 is %d %d\n", depth.at<unsigned short>(y, x), depth2.at<unsigned short>(y, x));
+                    }
+                    else
+                    {
+                        printf("read write ok!\n");
+                    }
+                }
+            }
+            break;
+
+        }
